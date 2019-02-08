cf41c18484173e7b6a79d950263aca00a2fcc7cc
DMamak <damianmamak1996@gmail.com>
2019-02-08 14:09:02 +0000

    added fab

--- app/src/main/java/com/example/concertmate/ConcertFragment.java
+++ app/src/main/java/com/example/concertmate/ConcertFragment.java
@@ -2,6 +2,7 @@ package com.example.concertmate;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -38,6 +40,14 @@ public class ConcertFragment extends Fragment {
         Bundle bundle = this.getArguments();
         int check = bundle.getInt("position");
         RecyclerView mRecycler = view.findViewById(R.id.recycler);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"YA CLICKED ME MATE!!!!",Toast.LENGTH_LONG).show();
+            }
+        });
+
         adapter = new concertAdapterView(concertList);
         mRecycler.setAdapter(adapter);
         RecyclerView.LayoutManager mLayoutManager =new LinearLayoutManager(getActivity());
