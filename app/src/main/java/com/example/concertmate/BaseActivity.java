687c6a8b77f2afb3ed3eb437d79740c99af98c9f
DMamak <damianmamak1996@gmail.com>
2019-02-14 12:41:37 +0000

    move fab to recycler fragment

--- app/src/main/java/com/example/concertmate/BaseActivity.java
+++ app/src/main/java/com/example/concertmate/BaseActivity.java
@@ -11,18 +11,6 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FilterFragment filterFragment = FilterFragment.newInstance(); //get a new Fragment instance
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.mainFragment, filterFragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
         TabFragment tabFragment= TabFragment.newInstance(); //get a new Fragment instance
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.mainFragment, tabFragment)
--- app/src/main/java/com/example/concertmate/ConcertFragment.java
+++ app/src/main/java/com/example/concertmate/ConcertFragment.java
@@ -2,6 +2,7 @@ package com.example.concertmate;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,6 +21,17 @@ public class ConcertFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, Bundle savedInstanceState){
         Calendar c = Calendar.getInstance();
         View view = inflater.inflate(R.layout.recycler_fragment,container,false);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FilterFragment filterFragment = FilterFragment.newInstance(); //get a new Fragment instance
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.mainFragment, filterFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
         Bundle bundle = this.getArguments();
         int check = bundle.getInt("position");
         RecyclerView mRecycler = view.findViewById(R.id.recycler);
