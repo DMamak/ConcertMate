78ffe56e1663d1880aa8ca2910a5e647ccdae92f
DMamak <damianmamak1996@gmail.com>
2019-02-25 18:53:16 +0000

    added multi delete for notes

--- app/src/main/java/com/example/concertmate/Fragments/NotesFragment.java
+++ app/src/main/java/com/example/concertmate/Fragments/NotesFragment.java
@@ -3,28 +3,39 @@ package com.example.concertmate.Fragments;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
+import com.example.concertmate.Models.Concert;
 import com.example.concertmate.Models.Notes;
 import com.example.concertmate.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class NotesFragment extends BaseFragment {
+    private DatabaseReference mDatabase;
     ListView notesListView;
 
     public NotesFragment(){}
 
     public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.notes_list, container, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         notesListView = view.findViewById(R.id.notes_list_view);
+        final  CustomAdapter adapter = new CustomAdapter(getContext(), getJsonConcert().getNotesArrayList());
         notesListView.setOnItemClickListener(new OnItemClickListener(){
 
             @Override
@@ -34,14 +45,60 @@ public class NotesFragment extends BaseFragment {
                 goToEditNote(bundle);
             }
         });
-        notesListView.setOnLongClickListener(new View.OnLongClickListener() {
+
+        notesListView.setAdapter(adapter);
+        notesListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        notesListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
-            public boolean onLongClick(View v) {
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.menu_item_delete_notes, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
                 return false;
             }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.menu_item_delete_notes:
+                        deleteNotes(mode);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            private void deleteNotes(ActionMode actionMode)
+            {
+                Concert concert = getJsonConcert();
+                for (int i = adapter.getCount() - 1; i >= 0; i--)
+                {
+                    if (notesListView.isItemChecked(i))
+                    {
+                        concert.getNotesArrayList().remove(i);
+                        mDatabase.child("concert").child(concert.getId()).setValue(concert);
+                        adapter.getData().remove(i);
+                    }
+                }
+                actionMode.finish();
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
         });
-        CustomAdapter adapter = new CustomAdapter(getContext(), getJsonConcert().getNotesArrayList());
-        notesListView.setAdapter(adapter);
         return view;
     }
 
@@ -80,6 +137,10 @@ public class NotesFragment extends BaseFragment {
             Notes c = data.get(position);
             title.setText(c.getNotes());
             return vi;
+        }
+
+        public ArrayList<Notes> getData(){
+            return data;
         }
 
     }
